34766798eb9752f31f1ecba12c555f6b42f49aca
Hòa Nguyễn <shin.l0v3.ly@gmail.com>
2020-08-25 12:21:54 +0700

    Create New Project

---
+++ ChoQueVN/Controllers/LoginController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ChoQueVN.Modals;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ChoQueVN.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult LoginPost()
+        {
+            string _account = Request.Form["Account"];
+            string _password = Request.Form["Password"];
+
+            //ma hoa password
+            _password = Security.Encrypt(_password.ToString());
+
+            //đăng nhập thành công
+            //sét biến session  để kiểm tra các trang trong admin
+            HttpContext.Session.SetString("Account", _account);
+            return Redirect("/Admin/Home");
+        }
+    }
+}
---
+++ ChoQueVN/Middlewares/CheckLoginMiddleware.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+
+namespace ChoQueVN.Middlewares
+{
+    // You may need to install the Microsoft.AspNetCore.Http.Abstractions package into your project
+    public class CheckLoginMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public CheckLoginMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public Task Invoke(HttpContext httpContext)
+        {
+            //Lấy đường dẫn url
+            var path = httpContext.Request.Path.ToString();
+            //nếu path bắt đầu bằng chữ/admin thì kiểm tra xem session đã tồn tại chưa => nếu chưa tồn tại thì di chuyển đến trang login
+            path = path.ToLower();
+            if (path != null && path.StartsWith("/admin"))
+            {
+                if (httpContext.Session.GetString("Account") == null)
+                {
+                    //di chuyển đến trang login
+                    httpContext.Response.Redirect("/login");
+                }
+            }
+            return _next(httpContext);
+        }
+    }
+
+    // Extension method used to add the middleware to the HTTP request pipeline.
+    public static class CheckLoginMiddlewareExtensions
+    {
+        public static IApplicationBuilder UseCheckLoginMiddleware(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<CheckLoginMiddleware>();
+        }
+    }
+}
